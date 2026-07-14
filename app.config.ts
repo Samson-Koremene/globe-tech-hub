86ffb6e25b672735f226cca84de25bc757f341c6
Samson-Koremene <koremenesamson@gmail.com>
2026-07-14 18:39:51 +0100

    chore: suppress TS error for tanstack config import

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+// @ts-ignore: This module lacks type declarations in the current beta version but works at runtime
 import { defineConfig } from '@tanstack/react-start/config';
 
 export default defineConfig({
